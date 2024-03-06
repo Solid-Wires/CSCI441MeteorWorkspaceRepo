d60920499ee63d076a421fde54db69f5470a33de
Solid Wires <solid.wires@gmail.com>
2024-03-06 13:02:17 -0500

    Now it works

--- proj1/client/main.js
+++ proj1/client/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Meteor } from 'meteor/meteor';
-import './main.html'; // Why do I have to explicitly state this??
+//import './main.html';
 
 Meteor.startup(function () {
 
